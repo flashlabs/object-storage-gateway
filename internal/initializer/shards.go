cce55a3e3f0f2f44804fcc484dd7d8ceeabcb322
Marek Skopowski <marek.skopowski@flashlabs.pl>
2024-01-12 19:01:07 +0100

    List containers

--- internal/initializer/shards.go
+++ internal/initializer/shards.go
@@ -1,9 +1,30 @@
 package initializer
 
-import "log"
+import (
+	"context"
+	"fmt"
+	"log"
 
-func Shards() error {
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/client"
+)
+
+func Shards(c context.Context) error {
 	log.Println("Initializing shards")
+
+	cli, err := client.NewClientWithOpts(client.FromEnv)
+	if err != nil {
+		return fmt.Errorf("error while executing client.NewClientWithOpts: %w", err)
+	}
+
+	containers, err := cli.ContainerList(c, types.ContainerListOptions{})
+	if err != nil {
+		return fmt.Errorf("error while executing cli.ContainerList: %w", err)
+	}
+
+	for _, container := range containers {
+		fmt.Printf("%s %s\n", container.ID[:10], container.Image)
+	}
 
 	return nil
 }
