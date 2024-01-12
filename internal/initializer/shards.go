dcc831c5100fe26f3285cf6b30710c37f3d72703
Marek Skopowski <marek.skopowski@flashlabs.pl>
2024-01-12 20:18:02 +0100

    Read containers list

--- internal/initializer/shards.go
+++ internal/initializer/shards.go
@@ -4,9 +4,14 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"regexp"
 
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/client"
+)
+
+var (
+	reContainerNum = regexp.MustCompile(`[0-9]+`)
 )
 
 func Shards(c context.Context) error {
@@ -17,13 +22,18 @@ func Shards(c context.Context) error {
 		return fmt.Errorf("error while executing client.NewClientWithOpts: %w", err)
 	}
 
+	// TODO: filter by name
 	containers, err := cli.ContainerList(c, types.ContainerListOptions{})
 	if err != nil {
 		return fmt.Errorf("error while executing cli.ContainerList: %w", err)
 	}
 
 	for _, container := range containers {
-		fmt.Printf("%s %s\n", container.ID[:10], container.Image)
+		fmt.Printf("%s %s %s\n", container.ID[:10], container.Image, container.Names)
+
+		name := container.Names[0]
+
+		fmt.Println("name2 ", reContainerNum.FindString(name))
 	}
 
 	return nil
